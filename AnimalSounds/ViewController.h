11f04e27697a36501374d1d9f519bafe4919b2a8
mstfklinc <mustafakilinc429@gmail.com>
2021-09-23 15:04:00 +0300

    Edits for cat sounds

--- AnimalSounds/ViewController.h
+++ AnimalSounds/ViewController.h
@@ -6,8 +6,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    SystemSoundID catID;
+    SystemSoundID dogID;
+    SystemSoundID frogID;
+    SystemSoundID horseID;
+    SystemSoundID pigID;
+    SystemSoundID sheepID;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+
+- (IBAction)cat:(id)sender;
+- (IBAction)dog:(id)sender;
+- (IBAction)frog:(id)sender;
+- (IBAction)horse:(id)sender;
+- (IBAction)pig:(id)sender;
+- (IBAction)sheep:(id)sender;
 
 
 @end
--- AnimalSounds/ViewController.m
+++ AnimalSounds/ViewController.m
@@ -15,8 +15,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSURL *soundURL1 = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Cat" ofType:@"wav"]];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)soundURL1, &catID);
+    
 }
 
 
+- (IBAction)sheep:(id)sender {
+}
+
+- (IBAction)pig:(id)sender {
+}
+
+- (IBAction)horse:(id)sender {
+}
+
+- (IBAction)frog:(id)sender {
+}
+
+- (IBAction)dog:(id)sender {
+}
+
+- (IBAction)cat:(id)sender {
+    
+    AudioServicesPlaySystemSound(catID);
+    
+}
 @end
